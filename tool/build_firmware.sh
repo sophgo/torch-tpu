b97671a86bc28d19651cca6601ac241c4a379c3c
zhaoyang.wang <zhaoyang.wang@sophgo.com>
2022-12-29 21:12:00 +0800

    fix firmware build scripts
    Change-Id: Ifc4a82afce4fe5aab7924b14f70f11582b5305d3

--- tool/build_firmware.sh
+++ tool/build_firmware.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+echo "build firmware!"
+
 if [ "$#" -lt 1 ]; then
   echo "Usage: $0 "device_code_dir1,device_code_dir2" [output_dir]"
   exit -1
 fi
 
 DEVICE_DIR=$1
-OUTPUT_DIR=${2:-$TPUKERNEL_TOP/out}
+#OUTPUT_DIR=${2:-$TPUKERNEL_TOP/out}
+OUTPUT_DIR=${2:-$TRAIN_TOP/out}
 mkdir -p ${OUTPUT_DIR}/device
 
 CURRENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
 
 mkdir -p ${OUTPUT_DIR}
 IFS=',' read -ra DIRS<<< "${DEVICE_DIR}"
@@ -27,9 +31,9 @@ cp -r ${CURRENT_DIR}/target-ram.lds ${OUTPUT_DIR}
 pushd ${OUTPUT_DIR}
 make -f Makefile.device
 if [ $? -ne 0 ]; then echo "Failed to make firmware!"; exit -1; fi
-source ${CURRENT_DIR}/version_info.sh
-python3 ${CURRENT_DIR}/firmware_pack.py bm1684x.bin .
-rm -rf bm1684x.bin
-rm -rf build
-rm -rf device
+#source ${CURRENT_DIR}/version_info.sh
+#python3 ${CURRENT_DIR}/firmware_pack.py bm1684x.bin .
+#rm -rf bm1684x.bin
+#rm -rf build
+#rm -rf device
 popd
