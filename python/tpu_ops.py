d6a5b7939c3076bb96c9fefb0c9e35b9d644b47a
zhaoyang.wang <zhaoyang.wang@sophgo.com>
2023-03-08 17:35:27 +0800

    add FP16 conv forward&backward
    Change-Id: I4de37d70a03d709cc91f0a242f7d68305b81fc70

--- python/tpu_ops.py
+++ python/tpu_ops.py
@@ -58,6 +58,14 @@ class Conv2dFunc(Function):
         groups = ctx.groups
         grad_input = grad_weight = grad_bias = None
 
+        stride_h = get_parameter(stride, 0)
+        stride_w = get_parameter(stride, 1)
+        dilation_h = get_parameter(dilation, 0)
+        dilation_w = get_parameter(dilation, 1)
+        padding_h = get_parameter(padding, 0)
+        padding_w = get_parameter(padding, 1)
+
+        # Test FP16
         grad_out_np = np.asarray(grad_output.half().data.flatten())
         input_np = np.asarray(input.half().data.flatten())
         weight_np = np.asarray(weight.half().data.flatten())
@@ -66,50 +74,43 @@ class Conv2dFunc(Function):
         # just do reorder in here temporarily
         # can be deleted after when get weight from bmodel
         # [oc, ic, kh, kw] => [1, oc, DIV_UP(ic, 32) * kh * kw, 32]
-        oc = weight.shape[0]
-        ic = weight.shape[1]
-        kh = weight.shape[2]
-        kw = weight.shape[3]
-        weight_32IC = torch.zeros((oc * math.ceil(ic / 32) * kh * kw * 32,))
-        for oc_idx in range(oc):
-            for ic_idx in range(math.ceil(ic / 32)):
-                for k_idx in range(kh * kw):
-                    for inner in range(32):
-                        if ic_idx * 32 + inner >= ic :
-                            break
-                        src_idx = oc_idx * ic * kh * kw + (ic_idx * 32 + inner) * kh * kw + k_idx;
-                        dst_idx = oc_idx * math.ceil(ic / 32) * kh * kw * 32 + ic_idx * kh * kw * 32 + k_idx * 32 + inner;
-                        weight_32IC[dst_idx] = weight.data.flatten()[src_idx]
-        weight_32IC_np = np.asarray(weight_32IC.half().data)
+        #oc = weight.shape[0]
+        #ic = weight.shape[1]
+        #kh = weight.shape[2]
+        #kw = weight.shape[3]
+        #weight_32IC = torch.zeros((oc * math.ceil(ic / 32) * kh * kw * 32,))
+        #for oc_idx in range(oc):
+        #    for ic_idx in range(math.ceil(ic / 32)):
+        #        for k_idx in range(kh * kw):
+        #            for inner in range(32):
+        #                if ic_idx * 32 + inner >= ic :
+        #                    break
+        #                src_idx = oc_idx * ic * kh * kw + (ic_idx * 32 + inner) * kh * kw + k_idx;
+        #                dst_idx = oc_idx * math.ceil(ic / 32) * kh * kw * 32 + ic_idx * kh * kw * 32 + k_idx * 32 + inner;
+        #                weight_32IC[dst_idx] = weight.data.flatten()[src_idx]
+        #weight_32IC_np = np.asarray(weight_32IC.half().data)
 
         grad_input_np = np.ones(input.shape, dtype = np.float16)
         grad_weight_np = np.ones(weight.shape, dtype = np.float16)
-        grad_bias_np = np.ones((oc,), dtype = np.float16)
-
-        stride_h = get_parameter(stride, 0)
-        stride_w = get_parameter(stride, 1)
-        dilation_h = get_parameter(dilation, 0)
-        dilation_w = get_parameter(dilation, 1)
-        padding_h = get_parameter(padding, 0)
-        padding_w = get_parameter(padding, 1)
+        grad_bias_np = np.ones(bias.shape, dtype = np.float16)
 
         grad_bias_enable = 0 if bias is None else 1
-        sgdnn_pybind.conv_backward(grad_out_np,
-                                     input_np,
-                                     weight_32IC_np,
-                                     grad_input_np,
-                                     grad_weight_np,
-                                     grad_bias_np,
-                                     input.shape[0], input.shape[1],
-                                     input.shape[2], input.shape[3],
-                                     grad_output.shape[1],
-                                     grad_output.shape[2],
-                                     grad_output.shape[3],
-                                     groups,
-                                     weight.shape[2], weight.shape[3],
-                                     stride_h, stride_w, dilation_h, dilation_w,
-                                     padding_h, padding_h, padding_w, padding_w,
-                                     0, 1, 1, grad_bias_enable);
+        sgdnn_pybind.conv_backward_fp16(grad_out_np,
+                                        input_np,
+                                        weight_np,
+                                        grad_input_np,
+                                        grad_weight_np,
+                                        grad_bias_np,
+                                        input.shape[0], input.shape[1],
+                                        input.shape[2], input.shape[3],
+                                        grad_output.shape[1],
+                                        grad_output.shape[2],
+                                        grad_output.shape[3],
+                                        groups,
+                                        weight.shape[2], weight.shape[3],
+                                        stride_h, stride_w, dilation_h, dilation_w,
+                                        padding_h, padding_h, padding_w, padding_w,
+                                        0, 1, 1, grad_bias_enable);
 
         grad_input = torch.from_numpy(grad_input_np).reshape(input.shape)
         grad_weight = torch.from_numpy(grad_weight_np).reshape(weight.shape)
@@ -117,6 +118,38 @@ class Conv2dFunc(Function):
         #grad_weight = torch.transpose(grad_weight, 0, 1)
         if bias is not None:
             grad_bias = torch.from_numpy(grad_bias_np)
+
+        #Test FP32
+        #grad_out_np = np.asarray(grad_output.data.flatten())
+        #input_np = np.asarray(input.data.flatten())
+        #weight_np = np.asarray(weight.data.flatten())
+
+        #grad_input_np = np.ones(input.shape, dtype = np.float32)
+        #grad_weight_np = np.ones(weight.shape, dtype = np.float32)
+        #grad_bias_np = np.ones(bias.shape, dtype = np.float32)
+
+        #grad_bias_enable = 0 if bias is None else 1
+        #sgdnn_pybind.conv_backward(grad_out_np,
+        #                           input_np,
+        #                           weight_np,
+        #                           grad_input_np,
+        #                           grad_weight_np,
+        #                           grad_bias_np,
+        #                           input.shape[0], input.shape[1],
+        #                           input.shape[2], input.shape[3],
+        #                           grad_output.shape[1],
+        #                           grad_output.shape[2],
+        #                           grad_output.shape[3],
+        #                           groups,
+        #                           weight.shape[2], weight.shape[3],
+        #                           stride_h, stride_w, dilation_h, dilation_w,
+        #                           padding_h, padding_h, padding_w, padding_w,
+        #                           0, 1, 1, grad_bias_enable);
+
+        #grad_input = torch.from_numpy(grad_input_np).reshape(input.shape)
+        #grad_weight = torch.from_numpy(grad_weight_np).reshape(weight.shape)
+        #if bias is not None:
+        #    grad_bias = torch.from_numpy(grad_bias_np)
 
         return grad_input, grad_weight, grad_bias, None, None, None, None
 
