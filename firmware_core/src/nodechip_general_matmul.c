e6f814de298c4b6da0ae9e0abd3715b97fd6da47
ziyang.wang <ziyang.wang@sophgo.com>
2023-03-15 18:02:42 +0800

    add gemm sgdnn
    Change-Id: Ib8b813ba82dbc1f8d5d6c546fed9630df9d77b5f

---
+++ firmware_core/src/nodechip_general_matmul.c
@@ -0,0 +1,27 @@
+#include "nodechip_fc.h"
+#include "sg_api_struct.h"
+
+void tpu_kernel_api_general_matmul(const void *args)
+{
+    sg_api_general_matmul_t *api = (sg_api_general_matmul_t *)args;
+
+    tpu_initialize();
+    general_matmul(
+            api->L_global_addr,
+            api->R_global_addr,
+            api->Y_global_addr,
+            api->Y_global_addr,
+            1,
+            api->L_row_num,
+            api->L_col_num,
+            api->R_col_num,
+            api->R_transpose,
+            0,
+            tpu_type_convert(api->dtype),
+            tpu_type_convert(api->dtype),
+            tpu_type_convert(api->dtype),
+            0,
+            0);
+    tpu_poll();
+}
+TPUKERNEL_FUNC_REGISTER(tpu_kernel_api_general_matmul);
