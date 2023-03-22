e9c2b00aea38fae4f7a17f4196623b8a5c4ed3ac
zhaoyang.wang <zhaoyang.wang@sophgo.com>
2023-03-22 11:50:03 +0800

    use bmodel&flatbuffers in libsophon directly
    Change-Id: I2260acec89aae6568565ef6b33cb006f38158f39

--- scripts/prepare_toolchains.sh
+++ scripts/prepare_toolchains.sh
@@ -2,14 +2,15 @@
 CURRENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 if [ -z "$CROSS_TOOLCHAINS" ]; then
-  export CROSS_TOOLCHAINS=$CURRENT_DIR/../../toolchains_dir
+  export CROSS_TOOLCHAINS=$CURRENT_DIR/../toolchains_dir
 fi
 echo "export CROSS_TOOLCHAINS=$CROSS_TOOLCHAINS"
 
 mkdir -p $CROSS_TOOLCHAINS
 pushd $CROSS_TOOLCHAINS >>/dev/null
 
-arm_toolchain="gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf"
+#arm_toolchain="gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf"
+arm_toolchain="gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu"
 if [ ! -d "${arm_toolchain}" ]; then
     if [ ! -e "${arm_toolchain}.tar.xz" ]; then
         wget https://developer.arm.com/-/media/Files/downloads/gnu-a/10.3-2021.07/binrel/${arm_toolchain}.tar.xz
