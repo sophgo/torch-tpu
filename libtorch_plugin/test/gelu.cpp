9d28f1a3b4d79f99fa7b03653725d7a09eed2b38
ziyang.wang <ziyang.wang@sophgo.com>
2023-05-25 19:58:45 +0800

    Support local gelu fp16
    Change-Id: I2d43391f04a927b3974ecfee4ba33472190c60b3

--- libtorch_plugin/test/gelu.cpp
+++ libtorch_plugin/test/gelu.cpp
@@ -1,13 +1,21 @@
 #include <torch/torch.h>
 #include <TPUTorchUtils.h>
 
-static inline void test ( at::IntArrayRef input_shape )
+static inline void test ( at::IntArrayRef input_shape , bool use_half)
 {
   auto input_cpu = torch::randn ( input_shape ) - 0.5;
   auto input_tpu = input_cpu.to ( tpu::TPUGetCurrentDevice() );
+  if ( use_half )
+  {
+    input_tpu.to ( torch::kHalf );
+  }
   auto output_cpu = torch::gelu ( input_cpu );
   auto output_tpu = torch::gelu ( input_tpu );
   auto output_exp = output_cpu;
+  if ( use_half )
+  {
+    output_tpu.to ( torch::kFloat );
+  }
   auto output_got = output_tpu.to ( torch::Device ( "cpu" ) );
   std::cout << "Comparing gelu:"
             << " input shape = " << input_tpu.sizes()
@@ -19,17 +27,17 @@ static inline void test ( at::IntArrayRef input_shape )
 int main ()
 {
   const int batch = 64;
-  test ( { batch,   64, 112, 112 } ); // 0
-  test ( { batch,   64,  56,  56 } ); // 1
-  test ( { batch,  256,  56,  56 } ); // 2
-  test ( { batch,  128,  56,  56 } ); // 3
-  test ( { batch,  128,  28,  28 } ); // 4
-  test ( { batch,  512,  28,  28 } ); // 5
-  test ( { batch,  256,  28,  28 } ); // 6
-  test ( { batch,  256,  14,  14 } ); // 7
-  test ( { batch, 1024,  14,  14 } ); // 8
-  test ( { batch,  512,  14,  14 } ); // 9
-  test ( { batch,  512,   7,   7 } ); // 10
-  test ( { batch, 2048,   7,   7 } ); // 11
+  test ( { batch,   64, 112, 112 } , true ); // 0
+  test ( { batch,   64,  56,  56 } , false ); // 1
+  test ( { batch,  256,  56,  56 } , true ); // 2
+  test ( { batch,  128,  56,  56 } , true ); // 3
+  test ( { batch,  128,  28,  28 } , false ); // 4
+  test ( { batch,  512,  28,  28 } , false ); // 5
+  test ( { batch,  256,  28,  28 } , true ); // 6
+  test ( { batch,  256,  14,  14 } , true ); // 7
+  test ( { batch, 1024,  14,  14 } , false ); // 8
+  test ( { batch,  512,  14,  14 } , true ); // 9
+  test ( { batch,  512,   7,   7 } , true ); // 10
+  test ( { batch, 2048,   7,   7 } , false ); // 11
   return 0;
 }
