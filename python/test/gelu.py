48a3b754af46ff94868fc9c00ccb941d90572470
yu.hu <yu.hu@sophgo.com>
2023-05-26 17:20:54 +0800

    add some test scripts
    Change-Id: I486e48349949da85f5d34bc16b018fc6c5a79fa3

---
+++ python/test/gelu.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import copy
+
+torch.ops.load_library("../../libtorch_plugin/build/liblibtorch_plugin.so")
+torch.manual_seed(1000)
+
+if __name__ == "__main__":
+    device = "privateuseone"
+    batch = 8
+    sequence = 1024
+    hidden_size = 768
+
+    inp_cpu = torch.rand(batch, sequence, hidden_size)
+    inp_tpu = inp_cpu.to(device).half()
+
+    net_cpu = nn.GELU()
+    net_tpu = copy.deepcopy(net_cpu)
+    net_tpu = net_tpu.to(device).half()
+
+    out_cpu = net_cpu(inp_cpu)
+    out_tpu = net_tpu(inp_tpu)
+    out_tpu = out_tpu.float().to("cpu")
+    out_diff = out_cpu - out_tpu
+    print("cpu_out")
+    print(out_cpu)
+    print("tpu_out")
+    print(out_tpu)
+    
+    print (torch.max(abs(out_diff)))
---
+++ python/test/linear.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import copy
+
+torch.ops.load_library("../../libtorch_plugin/build/liblibtorch_plugin.so")
+torch.manual_seed(1000)
+
+if __name__ == "__main__":
+    device = "privateuseone"
+    batch = 8
+    sequence = 1024
+    hidden_size = 768
+    out_size = 3
+
+    inp_cpu = torch.rand(batch, sequence, hidden_size)
+    inp_tpu = inp_cpu.to(device).half()
+
+    ln_cpu = nn.Linear(hidden_size, out_size)
+    ln_tpu = copy.deepcopy(ln_cpu)
+    ln_tpu = ln_tpu.to(device).half()
+
+    out_cpu = ln_cpu(inp_cpu)
+    out_tpu = ln_tpu(inp_tpu)
+    out_tpu = out_tpu.float().to("cpu")
+    out_diff = out_cpu - out_tpu
+    print("cpu_out")
+    print(out_cpu)
+    print("tpu_out")
+    print(out_tpu)
+    
+    print (torch.max(abs(out_diff)))
