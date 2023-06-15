e62d139980ff9513ee3e4d3bd436de5211fe4f57
cen.li <cen.li@sophgo.com>
2023-06-15 11:02:17 +0800

    Softmax no max pivot
    Change-Id: I50b3d2cfcc157a6321320576ec0ee0420c6dc4f4

--- firmware_core/src/nodechip_softmax_forward_v2.c
+++ firmware_core/src/nodechip_softmax_forward_v2.c
@@ -3,7 +3,7 @@
 #include "common_def.h"
 #include "tpu_kernel.h"
 
-void nodechip_softmax_forward_2DR1_parallel (
+void nodechip_softmax_forward_2DR1_max_pivot_parallel (
 global_addr_t IGAddr,
 global_addr_t OGAddr,
 int Row, // row number
@@ -238,6 +238,221 @@ data_type_t DType ) // DT_FP32 or DT_FP16
         dim4 MStride; tpu_aligned_stride ( &MStride, 0, &RShape, DType );
         dim4 MBStride = { .n = MStride.n, .c = MStride.c, 0, 0 };
         tpu_bdc_fp_sub ( IAddrs[Index], IAddrs[Index], MAddr, &TShape, NULL, NULL, &MBStride, DType );
+        // input FP16 -> FP32
+        if ( DType == DT_FP16 )
+        {
+          tpu_bdc_cast ( IFP32Addrs[Index], IAddrs[Index], &TShape, NULL, NULL, DT_FP32, DT_FP16, RM_HALF_TO_EVEN );
+        }
+        // input = exp ( input )
+        tpu_bdc_fp32_exp ( IFP32Addrs[Index], IFP32Addrs[Index], W0Addr, W1Addr, ECAddr, ETAddr, &TShape );
+      }
+      // input = input * input_sum
+      dim4 SStride; tpu_aligned_stride ( &SStride, 0, &RShape, DT_FP32 );
+      dim4 SBStride = { .n = SStride.n, .c = SStride.c, 0, 0 };
+      tpu_bdc_fp_mul ( IFP32Addrs[Index], IFP32Addrs[Index], SAddr, &TShape, NULL, NULL, &SBStride, DT_FP32 );
+      // input FP32 -> FP16 (inplace)
+      if ( DType == DT_FP16 )
+      {
+        tpu_bdc_cast ( IFP32Addrs[Index], IFP32Addrs[Index], &TShape, NULL, NULL, DT_FP16, DT_FP32, RM_HALF_TO_EVEN );
+      }
+      // Move output from local memory to global memory
+      dim4 TStride; tpu_aligned_stride ( &TStride, 0, &TShape, DType );
+      dim4 Stride = { .n = TStride.n, .c = TStride.c, .h = 1, .w = 1 };
+      L2STodo = true;
+      L2SGAddr = OGAddr + ( CDone * OGStride.c + HDone * OGStride.h ) * DSize;
+      L2SLAddr = IFP32Addrs[Index];
+      L2SShape = Shape;
+      L2SGStride = OGStride;
+      L2SLStride = Stride;
+      HTodo -= Shape.h;
+      HDone += Shape.h;
+      Index = 1 - Index;
+    }
+    CTodo -= Shape.c;
+    CDone += Shape.c;
+  }
+  if ( tpu_is_parallel_state() )
+  {
+    tpu_parallel_end();
+  }
+  if ( L2STodo )
+  {
+    tpu_gdma_cpy_L2S ( L2SGAddr, L2SLAddr, &L2SShape, &L2SGStride, &L2SLStride, DType );
+    L2STodo = false;
+  }
+}
+
+void nodechip_softmax_forward_2DR1_parallel (
+global_addr_t IGAddr,
+global_addr_t OGAddr,
+int Row, // row number
+int Column, // column number
+data_type_t DType ) // DT_FP32 or DT_FP16
+{
+  const int DSize = tpu_data_type_size ( DType );
+  const int Tile = tpu_eu_num ( DType );
+  const dim4 TotalShape = { .n = 1, .c = Row, .h = Column, .w = 1 };
+  dim4 IGStride, OGStride;
+  tpu_continuous_stride ( &IGStride, &TotalShape );
+  tpu_continuous_stride ( &OGStride, &TotalShape );
+  const dim2 StrideOne = { .h = 1, .w = 1 };
+  const dim2 DilationOne = { .h = 1, .w = 1 };
+  const padding_t ZeroPad = { .top = 0, .left = 0, .bottom = 0, .right = 0 };
+  const scalar_t Zero = { .u32 = 0 };
+  const scalar_t OneFP32 = { .f32 = 1.f };
+  local_addr_t IAddrs[2], RTAddr, RSAddrs[2];
+  local_addr_t IFP32Addrs[2], W0Addr, W1Addr;
+  int CMax = Row, HMax = Column;
+  local_addr_t ETAddr = 0;
+  local_addr_t ECAddr = ETAddr + tpu_aligned_feature_size ( 1, 192, DT_FP32 );
+  IAddrs[0] = ECAddr + tpu_aligned_feature_size ( 1, 32, DT_FP32 );
+  while ( true )
+  {
+    int Size = DIV_UP ( CMax, NPU_NUM ) * tpu_aligned_feature_size ( DIV_UP ( HMax, Tile ), Tile, DType );
+    int SizeFP32 = DIV_UP ( CMax, NPU_NUM ) * tpu_aligned_feature_size ( DIV_UP ( HMax, Tile ), Tile, DT_FP32 );
+    int TSizeFP32 = DIV_UP ( CMax, NPU_NUM ) * tpu_aligned_feature_size ( 1, Tile, DT_FP32 );
+    int SSizeFP32 = DIV_UP ( CMax, NPU_NUM ) * tpu_aligned_feature_size ( 1, 1, DT_FP32 );
+    int Next = IAddrs[0] + Size;
+    IAddrs[1] = Next; Next = IAddrs[1] + Size;
+    if ( DType == DT_FP16 )
+    {
+      IFP32Addrs[0] = Next; Next = IFP32Addrs[0] + SizeFP32;
+      IFP32Addrs[1] = Next; Next = IFP32Addrs[1] + SizeFP32;
+    }
+    else
+    {
+      IFP32Addrs[0] = IAddrs[0];
+      IFP32Addrs[1] = IAddrs[1];
+    }
+    RTAddr = Next; Next = RTAddr + TSizeFP32;
+    RSAddrs[0] = Next; Next = RSAddrs[0] + SSizeFP32;
+    if ( HMax != Column )
+    {
+      RSAddrs[1] = Next; Next = RSAddrs[1] + SSizeFP32;
+    }
+    else
+    {
+      RSAddrs[1] = RSAddrs[0];
+    }
+    W0Addr = Next; Next = W0Addr + SizeFP32;
+    W1Addr = Next; Next = W1Addr + SizeFP32;
+    if ( Next <= LOCAL_MEM_SIZE )
+    {
+      break;
+    }
+    else
+    {
+      if ( CMax > NPU_NUM )
+      {
+        CMax -= NPU_NUM;
+        continue;
+      }
+      else if ( HMax > 1 )
+      {
+        HMax -= Tile;
+        continue;
+      }
+      else
+      {
+        TPUKERNEL_ASSERT ( false );
+      }
+    }
+  }
+  tpu_bdc_load_fp32_exp_table ( ETAddr );
+  tpu_bdc_load_fp32_exp_coeff ( ECAddr );
+  bool L2STodo = false;
+  global_addr_t L2SGAddr = 0;
+  local_addr_t L2SLAddr = 0;
+  dim4 L2SShape, L2SGStride, L2SLStride;
+  dim4 Shape = { .n = 1, .w = 1 };
+  int Index = 0; // Ping-pong buffer switching index
+  int CTodo = Row;
+  int CDone = 0;
+  while ( CTodo != 0 )
+  {
+    Shape.c = MIN ( CMax, CTodo );
+    dim4 RShape = { .n = Shape.n, .c = Shape.c, .h = 1, .w = 1 };
+    int HTodo = Column, HDone = 0;
+    /*
+     * input = exp ( input )
+     * input_sum = sum_row ( input )
+     */
+    while ( HTodo != 0 )
+    {
+      Shape.h = MIN ( HMax, HTodo );
+      dim4 TShape = { .n = Shape.n, .c = Shape.c, .h = DIV_UP ( Shape.h, Tile ), .w = Tile };
+      // Move input from global memory to local memory
+      dim4 TStride; tpu_aligned_stride ( &TStride, 0, &TShape, DType );
+      dim4 Stride = { .n = TStride.n, .c = TStride.c, .h = 1, .w = 1 };
+      tpu_gdma_cpy_S2L ( IAddrs[Index], IGAddr + ( CDone * IGStride.c + HDone * IGStride.h ) * DSize, &Shape, &Stride, &IGStride, DType );
+      if ( tpu_is_parallel_state() )
+      {
+        tpu_parallel_end();
+      }
+      tpu_parallel_start();
+      if ( L2STodo )
+      {
+        tpu_gdma_cpy_L2S ( L2SGAddr, L2SLAddr, &L2SShape, &L2SGStride, &L2SLStride, DType );
+        L2STodo = false;
+      }
+      // input FP16 -> FP32
+      if ( DType == DT_FP16 )
+      {
+        tpu_bdc_cast ( IFP32Addrs[Index], IAddrs[Index], &TShape, NULL, NULL, DT_FP32, DT_FP16, RM_HALF_TO_EVEN );
+      }
+      // input = exp ( input )
+      tpu_bdc_fp32_exp ( IFP32Addrs[Index], IFP32Addrs[Index], W0Addr, W1Addr, ECAddr, ETAddr, &TShape );
+      // set input tail
+      if ( Shape.h % Tile != 0 )
+      {
+        dim4 TStride; tpu_aligned_stride ( &TStride, 0, &TShape, DT_FP32 );
+        dim4 SShape = { .n = Shape.n, .c = Shape.c, .h = 1, .w = Tile - ( Shape.h % Tile ) };
+        tpu_bdc_set_C ( IFP32Addrs[Index] + Shape.h * 4, Zero, &SShape, &TStride, DT_FP32 );
+      }
+      // [ 1, row, DIV_UP ( column, Tile ), Tile ] -> [ 1, row, 1, Tile ] -> [ 1, row, 1, 1 ]
+      dim2 Kernel = { .h = TShape.h, .w = 1 };
+      tpu_bdc_fp_avg_pool2d ( RTAddr, IFP32Addrs[Index], &TShape, &Kernel, &ZeroPad, &StrideOne, &DilationOne, DT_FP32, OneFP32 );
+      TShape.h = 1; Kernel.h = 1; Kernel.w = TShape.w;
+      tpu_bdc_fp_avg_pool2d ( RSAddrs[Index], RTAddr, &TShape, &Kernel, &ZeroPad, &StrideOne, &DilationOne, DT_FP32, OneFP32 );
+      if ( HDone > 0 )
+      {
+        tpu_bdc_fp_add ( RSAddrs[Index], RSAddrs[Index], RSAddrs[1 - Index], &RShape, NULL, NULL, NULL, DT_FP32 );
+      }
+      HTodo -= Shape.h;
+      HDone += Shape.h;
+      Index = 1 - Index;
+    }
+    local_addr_t SAddr = RSAddrs[1 - Index];
+    // input_sum = 1 / input_sum
+    tpu_bdc_fp32_reciprocal ( SAddr, SAddr, &RShape, NULL, NULL );
+    HTodo = Column; HDone = 0;
+    /*
+     * input = input * input_sum
+     */
+    if ( HMax == Column )
+    {
+      Index = 1 - Index;
+    }
+    while ( HTodo != 0 )
+    {
+      Shape.h = MIN ( HMax, HTodo );
+      dim4 TShape = { .n = Shape.n, .c = Shape.c, .h = DIV_UP ( Shape.h, Tile ), .w = Tile };
+      if ( HMax != Column )
+      {
+        // Move input from global memory to local memory
+        dim4 TStride; tpu_aligned_stride ( &TStride, 0, &TShape, DType );
+        dim4 Stride = { .n = TStride.n, .c = TStride.c, .h = 1, .w = 1 };
+        tpu_gdma_cpy_S2L ( IAddrs[Index], IGAddr + ( CDone * IGStride.c + HDone * IGStride.h ) * DSize, &Shape, &Stride, &IGStride, DType );
+        if ( tpu_is_parallel_state() )
+        {
+          tpu_parallel_end();
+        }
+        tpu_parallel_start();
+        if ( L2STodo )
+        {
+          tpu_gdma_cpy_L2S ( L2SGAddr, L2SLAddr, &L2SShape, &L2SGStride, &L2SLStride, DType );
+          L2STodo = false;
+        }
         // input FP16 -> FP32
         if ( DType == DT_FP16 )
         {
