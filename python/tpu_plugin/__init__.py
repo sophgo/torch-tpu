13eeab748a941197d8290407498ea405b714985c
yu.hu <yu.hu@sophgo.com>
2023-07-20 11:55:35 +0800

    tpu_plugin support Optimer
    Change-Id: I8e6eb7d010b767d1484dbace457c5bddd245deee

--- python/tpu_plugin/__init__.py
+++ python/tpu_plugin/__init__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import torch.nn
+import ctypes as ct
 
 # setup envs
 here = os.path.dirname(os.path.abspath(__file__))
@@ -20,3 +21,14 @@ torch.ops.load_library(TPU_PLUGIN_PATH)
 from . import samples
 def TPU(dev_id = 0):
   return torch.device(f"privateuseone:{dev_id}")
+
+class OPTimer:
+    def __init__(self, libpath = TPU_PLUGIN_PATH):
+        self.lib_path = libpath
+        self._lib = ct.cdll.LoadLibrary(libpath)
+
+    def reset(self):
+        self._lib.tpu_op_timer_reset()
+
+    def dump(self):
+        self._lib.tpu_op_timer_dump()
