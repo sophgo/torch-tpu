6f349cf49088b96800266a9955b876baf4036e60
haozhe.yang <haozhe.yang@sophgo.com>
2023-09-21 17:53:09 +0800

    fix repeat bug
    Change-Id: I20016de441680b01ddbe68d03c6124853c45193b

--- libtorch_plugin/ops/Repeat.cpp
+++ libtorch_plugin/ops/Repeat.cpp
@@ -10,18 +10,38 @@
 
 namespace at {
 
+Tensor &repeat_out_tpu(const Tensor &self, const IntArrayRef repeats,
+                       Tensor &out) {
+  Tensor contiguous_self = self.is_contiguous() ? self : self.contiguous();
+  std::vector<int> repeat_times;
+  if (self.dim() == 0) {
+    Tensor out_cpu = out.cpu();
+    repeat_out(out_cpu, self.cpu(), repeats);
+    tpu::TPUCopyHostToDevice(out.data_ptr(), out_cpu.contiguous().data_ptr(),
+                             out.nbytes());
+  } else {
+    for (int i = 0; i < repeats.size(); ++i) {
+      repeat_times.push_back((int)repeats[i]);
+    }
+
+    TIMING_START
+    bm_status_t status = sgdnnRepeat(
+        tpu::TPUGetDeviceHandle(), tpu::TPUGenerateSgdnnTensor(contiguous_self),
+        repeat_times.data(), repeats.size(), tpu::TPUGenerateSgdnnTensor(out));
+    TORCH_CHECK(status == BM_SUCCESS);
+    TIMING_END(tpu::REPEAT)
+  }
+
+  return out;
+}
 //  - func: repeat(Tensor self, SymInt[] repeats) -> Tensor
 Tensor repeat_tpu(const Tensor &self, const IntArrayRef repeats) {
   if (self.dim() > 0) {
     CHECK_TENSOR_IN_DEVICE_NO_CONTIGUOUS(self);
     TORCH_CHECK(repeats.size() >= self.dim());
   }
-  // some op lead to non-contiguous tensor, so we need to make it contiguous
-  Tensor contiguous_self = self.is_contiguous() ? self : self.contiguous();
 
-  torch::Device device(torch::kPrivateUse1);
   IntArrayRef size(repeats);
-  std::vector<int> repeat_times;
   std::vector<int64_t> size_vec(size.begin(), size.end());
   if (repeats.size() == self.dim()) {
     for (int i = 0; i < self.dim(); ++i) {
@@ -38,28 +58,13 @@ Tensor repeat_tpu(const Tensor &self, const IntArrayRef repeats) {
     }
   }
   size = torch::IntArrayRef(size_vec);
-  Tensor out = torch::empty(size, self.dtype()).to(device);
-
-  if (self.dim() == 0) {
-    Tensor out_cpu;
-    repeat_out(out_cpu, self.cpu(), repeats);
-    tpu::TPUCopyDeviceToDevice(out.data_ptr(), out_cpu.contiguous().data_ptr(),
-                               out.nbytes());
-  }
-
-  for (int i = 0; i < repeats.size(); ++i) {
-    repeat_times.push_back((int)repeats[i]);
-  }
-
-  TIMING_START
-  bm_status_t status = sgdnnRepeat(
-      tpu::TPUGetDeviceHandle(), tpu::TPUGenerateSgdnnTensor(contiguous_self),
-      repeat_times.data(), repeats.size(), tpu::TPUGenerateSgdnnTensor(out));
-  TORCH_CHECK(status == BM_SUCCESS);
-  TIMING_END(tpu::REPEAT)
-
+  auto out = empty(size, self.options());
+  repeat_out_tpu(self, repeats, out);
   return out;
 }
-TORCH_LIBRARY_IMPL(aten, TPU, m) { m.impl("repeat", repeat_tpu); }
+TORCH_LIBRARY_IMPL(aten, TPU, m) {
+  m.impl("repeat.out", repeat_out_tpu);
+  m.impl("repeat", repeat_tpu);
+}
 
 } // namespace at
