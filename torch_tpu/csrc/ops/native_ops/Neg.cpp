d596a241c5177244eca7af03bb82f4fc6c121397
wei.shi <wei.shi@sophgo.com>
2023-12-12 18:26:49 +0800

    fix neg op
    Change-Id: I1dc02b9d49552ff588f1623c8631bb83a353fd9d

--- torch_tpu/csrc/ops/native_ops/Neg.cpp
+++ torch_tpu/csrc/ops/native_ops/Neg.cpp
@@ -1,50 +1,36 @@
-#include <torch/library.h>
-#include <torch/torch.h>
-#include <ATen/core/TensorBase.h>
+#include "common/config.h"
 #include <ATen/EmptyTensor.h>
+#include <ATen/core/TensorBase.h>
 #include <ATen/native/ConvUtils.h>
 #include <TPUDeviceManager.h>
 #include <TPUTorchUtils.h>
 #include <sgdnn_api.h>
-#include "common/config.h"
+#include <torch/library.h>
+#include <torch/torch.h>
 
+namespace at {
+Tensor &neg_out_tpu(const Tensor &self, Tensor &out) {
+  CHECK_TENSOR_IN_DEVICE(self);
+  CHECK_TENSOR_IN_DEVICE(out);
 
+  if (self.dim() == 0) {
+    auto out_cpu = neg(self.cpu());
+    tpu::TPUCopyHostToDevice(out.data_ptr(), out_cpu.contiguous().data_ptr(),
+                             out.nbytes());
+  }
 
-
-
-namespace at
-{
-Tensor & neg_out_tpu ( const Tensor & self, Tensor & out )
-{
-  CHECK_TENSOR_IN_DEVICE ( self );
-  CHECK_TENSOR_IN_DEVICE ( out );
-#ifdef SHOW_OP_INFO
-  static int count = 0;
-  std::cout << "neg_out " << count << std::endl;
-  ++count;
-#endif
-#if 0
-  auto out_cpu = neg( self.cpu());
-  tpu::TPUCopyHostToDevice ( out.data_ptr(), out_cpu.contiguous().data_ptr(), out.nbytes() );
-#endif
 #ifdef TPU_OP_TIMING
   auto timer = tpu::Timer().Start();
 #endif
-  bm_status_t status = sgdnnNeg(
-                       tpu::TPUGetDeviceHandle(),
-                       tpu::TPUGenerateSgdnnTensor ( self ),
-                    
-                       tpu::TPUGenerateSgdnnTensor ( out ) );
-  TORCH_CHECK ( status == BM_SUCCESS );
+  bm_status_t status =
+      sgdnnNeg(tpu::TPUGetDeviceHandle(), tpu::TPUGenerateSgdnnTensor(self),
+               tpu::TPUGenerateSgdnnTensor(out));
+  TORCH_CHECK(status == BM_SUCCESS);
 #ifdef TPU_OP_TIMING
-  tpu::OpTimer::Instance().AddTime ( tpu::NEG, timer.ElapsedUS() );
+  tpu::OpTimer::Instance().AddTime(tpu::NEG, timer.ElapsedUS());
 #endif
 
   return out;
 }
-TORCH_LIBRARY_IMPL ( aten, TPU, m )
-{
-  m.impl ( "neg.out", neg_out_tpu );
-  
-}
+TORCH_LIBRARY_IMPL(aten, TPU, m) { m.impl("neg.out", neg_out_tpu); }
 } // namespace at
