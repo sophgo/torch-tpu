0432f9385b42d1c5b68fa14b874a8698694e2335
yu.hu <yu.hu@sophgo.com>
2023-12-21 15:03:55 +0800

    lerp use cpu
    Change-Id: Ibf94ebbccca85e68193d55b898adb9637b96d1d0

--- torch_tpu/csrc/ops/native_ops/Lerp.cpp
+++ torch_tpu/csrc/ops/native_ops/Lerp.cpp
@@ -14,14 +14,16 @@ Tensor & lerp_scalar_out_tpu(const at::Tensor & self, const at::Tensor & end, co
   CHECK_TENSOR_IN_DEVICE( out );
   CHECK_TENSOR_IN_DEVICE( self );
   CHECK_TENSOR_IN_DEVICE( end );
-#if 0
+#if 1
     CPU_IMPL_WARNING();
     TIMING_START;
     auto out_cpu = lerp(self.cpu(), end.cpu(), weight);
     tpu::TPUCopyHostToDevice(out.data_ptr(), out_cpu.data_ptr(), out.nbytes());
     TIMING_END(tpu::CPU_LAYER);
 #else
-    out = self + weight * (end - self); //TODO
+    // THIS IMPL SLOW THAN CPU, AND HAS SOME ERRORS MAYBE, will cause grad update failed, seems like
+    //out = self + weight * ( end - self );
+    out = (1.0 - weight.toDouble()) * self + weight * end; //TODO:
 #endif
   SHOW_TENSOR_OP(self, end, out);
   return out;
