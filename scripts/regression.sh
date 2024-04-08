360d6d9c884db88a547068f441b617162cef68ca
da.teng <da.teng@sophgo.com>
2024-04-08 15:34:15 +0800

    add daily regression test
    Change-Id: I01497a218e864a30c4934111e0eee3dee4f96ae4

--- scripts/regression.sh
+++ scripts/regression.sh
@@ -18,6 +18,23 @@ function ops_utest() {
     return $cmd_utest_result
 }
 
+function gpt3block_test() {
+    start_time=$(date +%s) 
+    CURRENT_DIR=$(dirname ${BASH_SOURCE})
+    PYTHON_TEST_PATH=$CURRENT_DIR/../python/gen_ins/
+    echo "[INFO]PYTHON_TEST_PATH:$PYTHON_TEST_PATH"
+    pushd $PYTHON_TEST_PATH
+    export FORBID_CMD_EXECUTE=0
+    export CMODEL_FAST_EXEC=1
+    cmd_gpt3block_test="python3 gpt3block_TP16_fb.py"
+    $cmd_gpt3block_test; cmd_gpt3block_test_result=$?
+    echo "[INFO]cmd_gpt3block_test_result:$cmd_gpt3block_test_result"
+    popd
+    echo "*********** GPT3BLOCK TEST ENDED ***************"
+    end_time=$(date +%s)
+    echo "Time elapsed: $(($end_time - $start_time)) seconds"
+    return $cmd_gpt3block_test_result
+}
 
 export stable_libsophon_path="/workspace/libsophon_Release_20230605_025400"
 # function for online regression
@@ -158,6 +175,63 @@ function run_online_regression_test() {
   fi
 }
 
+function run_daily_regression_test() {
+  echo "[INFO]Ubuntu version"
+  version_cmd="cat /etc/os-release"
+  $version_cmd
+  CURRENT_DIR=$(dirname ${BASH_SOURCE})
+  echo "********************************************"
+  echo "[NOTE]Print_necessary_info"
+  echo "[INFO]CURRENT_DIR:$CURRENT_DIR"
+  
+  
+  test_CHIP_ARCH=${1:-bm1684x}
+  LIBSOPHON_LINK_PATTERN=${2:-local} #local or stable
+  TEST_PATTERN=${3:-online} #online,local, or fast
+  DEB_PATH_STABLE=${4:-none} #none or given path
+  VERSION_PATH_STABLE=${5:-0.4.8}
+
+  echo "[INFO]test_CHIP_ARCH:$test_CHIP_ARCH"
+  echo "[INFO]LIBSOPHON_LINK_PATTERN=$LIBSOPHON_LINK_PATTERN"
+  echo "[INFO]TEST_PATTERN=$TEST_PATTERN"
+  link_libsophon $LIBSOPHON_LINK_PATTERN $DEB_PATH_STABLE $VERSION_PATH_STABLE; ret_libsophon=$?
+  if [ $ret_libsophon -eq 255 ]; then
+    echo "[INFO]test_CHIP_ARCH:$test_CHIP_ARCH libsophon setting failed!"
+  else
+    if [ $LIBSOPHON_LINK_PATTERN = 'stable' ];then
+      echo "[INFO]test_CHIP_ARCH:$test_CHIP_ARCH"
+      build_kernel_module_real $test_CHIP_ARCH
+    elif [ $LIBSOPHON_LINK_PATTERN = 'local' ];then
+      echo "************** $LIBSOPHON_LINK_PATTERN-LIBSOPHON IS REAEDY *********"
+      source  $CURRENT_DIR/envsetup.sh $test_CHIP_ARCH $LIBSOPHON_LINK_PATTERN
+      new_clean; new_build
+      TPU_TRAIN_CMODEL_PATH=$CURRENT_DIR/../build/Release/firmware_core/libcmodel.so
+      echo "[INFO]tpu_train_cmodel_path:$TPU_TRAIN_CMODEL_PATH"
+      set_cmodel_firmware $TPU_TRAIN_CMODEL_PATH
+      echo "*************** CMODEL IS SET *************"
+    fi
+    if [ $TEST_PATTERN = "online" ] || [ $TEST_PATTERN = "local" ];then
+      # build_libtorch_plugin $TEST_PATTERN
+      echo "*************** LIBTORCH_PLUGIN IS BUILT *************"
+      ops_utest; ret_ops_utest=$?
+      echo "[INFO]ret_ops_utest:$ret_ops_utest"
+      if [ $ret_ops_utest -eq 0 ];then #must return [0,255] otherwise it will cause scripts fault early
+        echo "[RESULT-$test_CHIP_ARCH] all ops_utest are computed, Please check Results above"
+      else
+        echo "[RESULT-$test_CHIP_ARCH] some ops_utest are failed!"
+      fi
+      if [ "${test_CHIP_ARCH}" = "sg2260" ]; then
+        gpt3block_test; ret_gpt3block_test=$?
+        echo "[INFO]ret_gpt3block_test:$ret_gpt3block_test"
+        if [ $ret_gpt3block_test -eq 0 ];then #must return [0,255] otherwise it will cause scripts fault early
+          echo "[RESULT-$test_CHIP_ARCH] all gpt3block results are computed, Please check Results above"
+        else
+          echo "[RESULT-$test_CHIP_ARCH] some gpt3block results are failed!"
+        fi
+      fi
+    fi
+  fi
+}
 
 function fast_build_bm1684x_stable() {
   DEB_PATH_STABLE=${1:-none}
