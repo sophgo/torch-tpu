8333be702b7eb585814d9139d40582d6360595b8
yifei.gao <yifei.gao@sophgo.com>
2024-09-05 17:30:55 +0800

    [sg2260]:refine pmu & support cdma pmu
    Change-Id: I3102d9b0db9c7d5dc9728410841572f030bfb569

--- firmware_core/src/nodechip_pmu.c
+++ firmware_core/src/nodechip_pmu.c
@@ -10,14 +10,6 @@ extern void enable_tpu_perf_monitor(void);
 int tpu_kernel_pmu(const void *api_buf) {
     sg_api_pmu_t *api = (sg_api_pmu_t *)api_buf;
     if(api->enable) {
-        scalar_t C = {.u32 = 0};
-        unsigned int size = 81 * 1024 * 1024;
-        tpu_gdma_system_set(tpu_core_index() * size, C, size / 4, DT_UINT32);
-        tpu_sync_all();
-        tpu_poll();
-        // disable first, make sure the counter is reset
-        // otherwise pmu data will be all zero
-        disable_tpu_perf_monitor();
         enable_tpu_perf_monitor();
     } else {
         disable_tpu_perf_monitor();
