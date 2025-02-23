af7498245ef1c0fa997e78598c97649913d467d2
wangyang.zuo <wangyang.zuo@sophgo.com>
2025-02-23 21:28:20 +0800

    fix 32ic->norm compute error
    Change-Id: I29c2119fbfb24eb983149161e9132ef4ba9098ca

---
+++ firmware_core/src/debug.h
@@ -0,0 +1,255 @@
+#pragma once
+#include "tpu_kernel.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+// assert
+#include <assert.h>
+// #include "sg_fp16.h"
+// https://wiki.sophgo.com/pages/viewpage.action?pageId=158406905
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// static void weight_reorder_multicore()
+typedef global_addr_t gaddr_t;
+typedef local_addr_t  laddr_t;
+typedef data_type_t   dtype_t;
+#define unuse(x)      (void)(x)
+
+#ifndef pipeline_move
+#define pipeline_move(array, num) do { \
+  for (int i = (int)num - 1; i > 0; i--) { \
+    array[i] = array[i - 1];\
+  }\
+} while(0)
+#endif
+
+typedef struct {
+    gaddr_t addr;
+    dim4    shape;
+    dim4    stride;
+    data_type_t dtype;
+} gtensor_t;
+
+typedef struct {
+    laddr_t     addr;
+    dim4        shape;
+    dim4        stride;
+    data_type_t dtype;
+} ltensor_t;
+
+#define init_lmem(ltensor) (ltensor).addr = 0;
+
+#define ltensor_size(ltensor) \
+    ((ltensor).shape.n * (ltensor).stride.n * tpu_data_type_size((ltensor).dtype))
+
+#define malloc_after(ltensor1, ltensor2, ALIGN_SIZE) \
+    do {                                              \
+        (ltensor1).addr      = ALIGN((ltensor2).addr + ltensor_size(ltensor2), ALIGN_SIZE);                       \
+        size_t total_size    = (ltensor1).addr + ltensor_size(ltensor1);                                          \
+        if((int)total_size >= LOCAL_MEM_SIZE){                                                                          \
+            printf("Local memory overflow, current size: %zu, total size: %u\n", total_size, LOCAL_MEM_SIZE);       \
+            printf("error tensor name: %s\n", #ltensor1);                                                         \
+            assert(0);                                                                                             \
+        }                                                                                                         \
+    } while (0)
+
+#define check_info(cond) \
+    do { \
+        if(cond){ \
+            printf("may error: '%s'; please check the code in %s:%d!\n", #cond, __FILE__, __LINE__); \
+        } \
+    } while (0)
+
+#define get_addr_bank_id(addr) ((addr) / BANK_SIZE)
+
+#define malloc_after_with_align(ltensor1, ltensor2, ltensor3) \
+    do {                                                                  \
+        laddr_t tmp_addr     = ALIGN((ltensor2).addr + ltensor_size(ltensor2), 64);                               \
+        laddr_t end_addr     = (ltensor3).addr + ltensor_size(ltensor3);                                          \
+        if( get_addr_bank_id(end_addr) == get_addr_bank_id(tmp_addr) ){                                           \
+            tmp_addr        += ALIGN(end_addr, BANK_SIZE);                                                        \
+        }                                                                                                         \
+        (ltensor1).addr      = tmp_addr;                                                                          \
+        size_t total_size    = (ltensor1).addr + ltensor_size(ltensor1);                                          \
+        if((int)total_size > LOCAL_MEM_SIZE){                                                                          \
+            printf("Local memory overflow, current size: %zu, total size: %u\n", total_size, LOCAL_MEM_SIZE);     \
+            printf("error tensor name: %s\n", #ltensor1);                                                      \
+            assert(0);                                                                                             \
+        }                                                                                   \
+    } while (0)
+
+#define only_if(cond, flag) \
+        if(cond && flag){ \
+            flag = false; \
+            continue;     \
+        }
+
+#define reuse_stage(cond, flag) only_if(cond, flag)
+
+#define malloc_after_default(ltensor1, ltensor2) malloc_after(ltensor1, ltensor2, 64)
+
+#define get_var_name(var) #var
+
+#define inner_loop(idx, total, step) \
+    for(int idx = 0; idx < total; idx += step)
+
+#define PARALLEL_CHECK \
+    bool __parallel_flag = tpu_is_parallel_state(); \
+    if(__parallel_flag){ \
+        tpu_parallel_end(); \
+    }
+
+#define PARALLEL_RECORY \
+    if(__parallel_flag){ \
+        tpu_parallel_start(); \
+    }
+
+static inline void stride_assign(ltensor_t *ltensor, int align_method){
+    // align_method: 0: align, 1: line align, 2: compact align
+    switch(align_method){
+        case 0:
+            ltensor->stride.w = 1;
+            ltensor->stride.h = ltensor->shape.w;
+            ltensor->stride.c = ALIGN(ltensor->shape.h * ltensor->stride.h, NPU_NUM / tpu_data_type_size(ltensor->dtype));
+            ltensor->stride.n = DIV_UP(ltensor->shape.c , NPU_NUM) * ltensor->stride.c;
+            break;
+        case 1:
+            ltensor->stride.w = 1;
+            ltensor->stride.h = ALIGN(ltensor->shape.w, 64 / tpu_data_type_size(ltensor->dtype));
+            ltensor->stride.c = ltensor->shape.h * ltensor->stride.h;
+            ltensor->stride.n = DIV_UP(ltensor->shape.c, 64) * ltensor->stride.c;
+            break;
+        case 2:
+            ltensor->stride.w = 1;
+            ltensor->stride.h = ltensor->shape.w;
+            ltensor->stride.c = ltensor->shape.h * ltensor->stride.h;
+            ltensor->stride.n = DIV_UP(ltensor->shape.c, 64) * ltensor->stride.c;
+            break;
+        default:
+            break;
+    }
+}
+
+static inline gaddr_t get_gtensor_idx_addr(gtensor_t *gtensor, dim4 slice){
+    size_t offset = slice.n * gtensor->stride.n * tpu_data_type_size(gtensor->dtype) +
+                    slice.c * gtensor->stride.c * tpu_data_type_size(gtensor->dtype) +
+                    slice.h * gtensor->stride.h * tpu_data_type_size(gtensor->dtype) +
+                    slice.w * gtensor->stride.w * tpu_data_type_size(gtensor->dtype);
+    return gtensor->addr + offset;
+}
+
+// [0,64, xxx,xxx]
+static inline laddr_t get_ltensor_idx_addr(ltensor_t *ltensor, dim4 slice){
+    int npu_start      = ltensor->addr / LOCAL_MEM_SIZE;
+    laddr_t basic_addr = ltensor->addr % LOCAL_MEM_SIZE;
+    int total_c_slice  = slice.c+npu_start;
+    return basic_addr + slice.n * ltensor->stride.n * tpu_data_type_size(ltensor->dtype) +
+                       ( total_c_slice / NPU_NUM) * ltensor->stride.c * tpu_data_type_size(ltensor->dtype) +
+                       (total_c_slice % NPU_NUM) * LOCAL_MEM_SIZE +
+                       slice.h * ltensor->stride.h * tpu_data_type_size(ltensor->dtype) +
+                       slice.w * ltensor->stride.w * tpu_data_type_size(ltensor->dtype);
+}
+
+static inline gtensor_t get_gtensor_slice(gtensor_t *gtensor, dim4 slice, dim4 shape){
+    gtensor_t slice_tensor = *gtensor;
+    slice_tensor.addr      = get_gtensor_idx_addr(gtensor, slice);
+    slice_tensor.shape     = shape;
+    return slice_tensor;
+}
+
+static inline ltensor_t get_ltensor_slice(ltensor_t *ltensor, dim4 slice, dim4 shape){
+    ltensor_t slice_tensor = *ltensor;
+    slice_tensor.addr      = get_ltensor_idx_addr(ltensor, slice);
+    slice_tensor.shape     = shape;
+    return slice_tensor;
+}
+
+static inline void global_stride(gtensor_t *gtensor){
+    gtensor->stride.w = 1;
+    gtensor->stride.h = gtensor->shape.w;
+    gtensor->stride.c = gtensor->shape.h * gtensor->stride.h;
+    gtensor->stride.n = gtensor->shape.c * gtensor->stride.c;
+}
+
+
+static inline ltensor_t init_ltensor(dim4 shape, data_type_t dtype){
+    ltensor_t ltensor = {0, shape, {0}, dtype};
+    stride_assign(&ltensor, 0);
+    return ltensor;
+}
+
+static inline gtensor_t init_gtensor(global_addr_t addr, dim4 shape, data_type_t dtype){
+    gtensor_t gtensor = {addr, shape, {0}, dtype};
+    global_stride(&gtensor);
+    return gtensor;
+}
+
+#define show_tensor(ltensor) \
+    do { \
+        printf(">>>>>>>>>>>>>>>>>> show %s\n", get_var_name(ltensor)); \
+        printf("core_id: %d\n", tpu_core_index()); \
+        printf("shape:  %d %d %d %d\n", ltensor.shape.n, ltensor.shape.c, ltensor.shape.h, ltensor.shape.w); \
+        printf("stride: %d %d %d %d\n", ltensor.stride.n, ltensor.stride.c, ltensor.stride.h, ltensor.stride.w); \
+        printf("dtype: %d\n", ltensor.dtype); \
+        printf("addr:  %llu\n", ltensor.addr); \
+        printf(">>>>>>>>>>>>>>>>>> end\n"); \
+    } while (0)
+
+// #define global_reshape(gtensor, new_shape) \
+//     do { \
+//         (gtensor).shape = new_shape; \
+//         global_stride(&gtensor); \
+//     } while (0)
+
+static inline void global_reshape(gtensor_t *gtensor, dim4 new_shape){
+    gtensor->shape = new_shape;
+    global_stride(gtensor);
+}
+
+static inline bool check_gtensor(gtensor_t gtensor0, gtensor_t gtensor1){
+
+    return gtensor0.addr == gtensor1.addr &&
+           gtensor0.shape.n == gtensor1.shape.n &&
+           gtensor0.shape.c == gtensor1.shape.c &&
+           gtensor0.shape.h == gtensor1.shape.h &&
+           gtensor0.shape.w == gtensor1.shape.w &&
+           gtensor0.stride.n == gtensor1.stride.n &&
+           gtensor0.stride.c == gtensor1.stride.c &&
+           gtensor0.stride.h == gtensor1.stride.h &&
+           gtensor0.stride.w == gtensor1.stride.w &&
+           gtensor0.dtype == gtensor1.dtype;
+}
+// if you want to open dump function, you should open in TPU1686 and copy so file into tputrain
+void logfordebug(const char* fmt, ...);
+
+void dump_local_data_into_file(local_addr_t addr, dim4* stride, dim4* shape, int size, const char* fname);
+void dump_local_data_into_file_with_idx(local_addr_t addr, dim4* stride, dim4* shape, int size, const char* fname, int idx);
+void dls(local_addr_t addr, int n, int c, int h, int w, int dflag, int layout, const char* fname);
+void dlsn(local_addr_t addr, int n, int c, int h, int w, int dflag, int layout, const char* fname, int idx);
+void dlsnn(local_addr_t addr, int n, int c, int h, int w, int dflag, int layout, const char* fname, int num, ...);
+void show_npu_data(local_addr_t addr, int num, int flag);
+void global2file(global_addr_t addr, int size, const char* fname);
+void global2file_with_shape(global_addr_t addr, int n, int c, int h, int w, int nstride, int cstride, int hstride, int wstride, int size, const char* fname);
+void dgsn(global_addr_t addr, int n, int c, int h, int w, int nstride, int cstride, int hstride, int wstride, int size, const char* fname, int idx);
+void dgssn(global_addr_t addr, int n, int c, int h, int w, int size, const char* fname, int idx);
+
+void dltensor(ltensor_t* ltensor, const char* name);
+void dltensor_with_idx(ltensor_t* ltensor, const char* name, int idx);
+void dltensor_with_2idx(ltensor_t* ltensor, const char* name, int idx1, int idx2);
+
+void dltensor_s(ltensor_t* ltensor, dim4 shape,  const char* name);
+void dltensor_s_with_idx(ltensor_t* ltensor, dim4 shape, const char* name, int idx);
+void dltensor_s_with_2idx(ltensor_t* ltensor, dim4 shape, const char* name, int idx1, int idx2);
+
+void dgtensor(gtensor_t* gtensor, const char* name);
+void dgtensor_with_idx(gtensor_t* gtensor, const char* name, int idx);
+void dgtensor_with_2idx(gtensor_t* gtensor, const char* name, int idx1, int idx2);
+
+void dgtensor_s(gtensor_t* gtensor, dim4 shape,  const char* name);
+void dgtensor_s_with_idx(gtensor_t* gtensor, dim4 shape, const char* name, int idx);
+void dgtensor_s_with_2idx(gtensor_t* gtensor, dim4 shape, const char* name, int idx1, int idx2);
+#ifdef __cplusplus
+}
+#endif
--- firmware_core/src/nodechip_conv_weight_recover.c
+++ firmware_core/src/nodechip_conv_weight_recover.c
@@ -1,6 +1,6 @@
 #include "sg_api_struct.h"
 #include "tpu_kernel.h"
-
+#include "debug.h"
 /*
  * [oc, kh * kw, DIV_UP(ic, 32), 32] => [oc, ic, kh, kw]
  *
@@ -139,19 +139,126 @@ int tpu_kernel_api_conv_weight_recover ( const void* args ) {
   return 0;
 }
 
+static void cpy_32ic_into_normal_in_local(ltensor_t* input, ltensor_t* output, int cur_slice){
+  // 单次考虑32个ic的结果 如果不满足32ic再说
+  int oc       = cur_slice;
+  int ic       = output->shape.h;
+  int khkw     = output->shape.w;
+  int cur_idx  = 0;
+  while(cur_idx * 32 < ic){
+    int remain_ic      = MIN(32, ic - cur_idx * 32);
+    laddr_t curi_laddr = get_ltensor_idx_addr(input,  (dim4){0, 0, cur_idx * khkw, 0});
+    laddr_t curo_laddr = get_ltensor_idx_addr(output, (dim4){0, 0, cur_idx * 32, 0});
+    // use bdc_cpy
+    dim4 target_shape  = {1, oc, remain_ic, khkw};
+    dim4 new_istride   = { input->stride.n, input->stride.c,  1, 32};
+    tpu_bdc_cpy(curo_laddr, curi_laddr, &target_shape, &(output->stride), &new_istride, input->dtype);
+    cur_idx ++;
+  }
+}
+
+// oc, ic_32, khkw, 32
+/**
+[oc, ic_32, khkw, 32] ddr -> [1, oc, ic_32 * kh * kw, 32]
+[1, oc, ic_32 * kh * kw, 32] - cpy -> [1, oc, ic_32 * 32, kh * kw] local
+*/
+void nodechip_conv_weight_32ic_to_normal_2(gaddr_t input, gaddr_t output, const dim4* shape){
+  // TODO: impl with different shape with better performance (such as ic is very large)
+  int oc    = shape->n;
+  int ic    = shape->c;
+  int kh    = shape->h;
+  int kw    = shape->w;
+  int dtype = DT_FP16;
+  // do not consider kh * kw == 1
+  int ic_32 = DIV_UP(ic, 32);
+  gtensor_t input_gtensor   = init_gtensor(input,  (dim4){oc, ic_32, kh * kw, 32}, dtype);
+  gtensor_t output_gtensor  = init_gtensor(output, (dim4){oc, ic, kh, kw},         dtype);
+
+  global_reshape(&input_gtensor,  (dim4){1, oc, ic_32 * kh * kw, 32});
+  global_reshape(&output_gtensor, (dim4){1, oc, ic, kh * kw });
+  show_tensor(input_gtensor);
+  show_tensor(output_gtensor);
+  int oc_slice    = NPU_NUM;
+
+  ltensor_t input_ltensor_1  = init_ltensor((dim4){1, oc_slice, ic_32 * kh * kw, 32}, dtype);
+  ltensor_t output_ltensor_1 = init_ltensor((dim4){1, oc_slice, ic, kh * kw},         dtype);
+  ltensor_t input_ltensor_2  = init_ltensor((dim4){1, oc_slice, ic_32 * kh * kw, 32}, dtype);
+  ltensor_t output_ltensor_2 = init_ltensor((dim4){1, oc_slice, ic, kh * kw},         dtype);
+
+  malloc_after(output_ltensor_1, input_ltensor_1, BANK_SIZE);
+  malloc_after(input_ltensor_2, output_ltensor_1, BANK_SIZE);
+  malloc_after(output_ltensor_2, input_ltensor_2, BANK_SIZE);
+
+  ltensor_t* load_tensors[2]  = {&input_ltensor_1, &input_ltensor_2};
+  ltensor_t* store_tensors[2] = {&output_ltensor_1, &output_ltensor_2};
+
+  int cur_idx[3]    = {0, 0, 0};
+  int cur_slice[3]  = {0, 0, 0};
+  int draning_idx   = 0;
+  int stage_idx     = 0;
+
+  while(cur_idx[2] < oc){
+    tpu_parallel_start();
+
+    if(draning_idx < 1){
+      cur_slice[0] = MIN(oc_slice - cur_idx[0], oc_slice);
+    }
+
+    if(stage_idx > 1){
+      // do store
+      ltensor_t* curo_ltensor  = store_tensors[stage_idx & 0x1];
+      gaddr_t cur_output_gaddr = get_gtensor_idx_addr(&output_gtensor, (dim4){0, cur_idx[2], 0, 0});
+      dim4 cur_store_shape     = {1, cur_slice[2], ic, kh * kw};
+      tpu_gdma_cpy_L2S(cur_output_gaddr, curo_ltensor->addr, &cur_store_shape, &(output_gtensor.stride), &(curo_ltensor->stride), dtype);
+    }
+
+    if(stage_idx > 0 && draning_idx < 2){
+      // do compute
+      ltensor_t* curi_ltensor = load_tensors[(stage_idx + 1) & 0x1];
+      ltensor_t* curo_ltensor = store_tensors[(stage_idx + 1) & 0x1];
+      cpy_32ic_into_normal_in_local(curi_ltensor, curo_ltensor, cur_slice[1]);
+    }
+
+    if(draning_idx < 1){
+      // do load
+      gaddr_t cur_input_gaddr = get_gtensor_idx_addr(&input_gtensor, (dim4){0, cur_idx[0], 0, 0});
+      ltensor_t* cur_ltensor  = load_tensors[stage_idx & 0x1];
+      dim4 cur_load_shape     = {1, cur_slice[0], ic_32 * kh * kw, 32};
+      tpu_gdma_cpy_S2L( cur_ltensor->addr, cur_input_gaddr, &cur_load_shape, &(cur_ltensor->stride), &(input_gtensor.stride), dtype);
+    }
+
+    tpu_parallel_end();
+
+    pipeline_move(cur_slice, 3);
+    pipeline_move(cur_idx, 3);
+
+    if(draning_idx < 1){
+      cur_idx[0] += cur_slice[0];
+      if(cur_idx[0] >= oc){
+        draning_idx ++;
+      }
+    }else{
+      draning_idx++;
+    }
+    stage_idx ++;
+  }
+
+}
+
 int tpu_kernel_api_conv_weight_recover_multi_core ( const void* args ) {
   sg_api_conv_weight_recover_t* api = ( sg_api_conv_weight_recover_t* ) args;
   tpu_initialize();
   int core_idx = tpu_core_index();
   int core_num = tpu_core_num();
-  int oc_slice = (api->shape[0] + core_num - 1) / core_num;
-  oc_slice     = (oc_slice + 31) / 32 * 32;
-  int ic_32    = (api->shape[1] + 31) / 32;
-  if(core_idx * oc_slice < api->shape[0]) {
-    dim4 shape = {MIN(oc_slice, api->shape[0] - core_idx * oc_slice), api->shape[1], api->shape[2], api->shape[3]};
+  int oc       = api->shape[0];
+  int ic       = api->shape[1];
+  int oc_slice = MAX(DIV_UP(oc, core_num), NPU_NUM);
+  int ic_32    = DIV_UP(ic, 32);
+  if(core_idx * oc_slice < oc) {
+    dim4 shape = {MIN(oc_slice, oc - core_idx * oc_slice), ic, api->shape[2], api->shape[3]};
     global_addr_t cur_input_gaddr  = api->input_global_addr  + core_idx * oc_slice * ic_32 * shape.h * shape.w * 32 * tpu_data_type_size(DT_FP16);
-    global_addr_t cur_output_gaddr = api->output_global_addr + core_idx * oc_slice * shape.h * shape.w * api->shape[1] * tpu_data_type_size(DT_FP16);
-    nodechip_conv_weight_32ic_to_normal_simluate(
+    global_addr_t cur_output_gaddr = api->output_global_addr + core_idx * oc_slice * shape.h * shape.w * ic * tpu_data_type_size(DT_FP16);
+    nodechip_conv_weight_32ic_to_normal_2(
       cur_input_gaddr,
       cur_output_gaddr,
       &shape
