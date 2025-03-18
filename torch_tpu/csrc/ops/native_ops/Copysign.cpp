7cbe0c34c4d33833de99f4675319681fc19b4f61
yu.hu <yu.hu@sophgo.com>
2025-03-18 16:45:57 +0800

    add copysign cpu impl
    Change-Id: I9dd7cf611b6c50bfe0f111d99f879c1fdc9f46af

---
+++ torch_tpu/csrc/ops/native_ops/Copysign.cpp
@@ -0,0 +1,47 @@
+#include <torch/library.h>
+#include <torch/torch.h>
+#include <ATen/core/TensorBase.h>
+#include <ATen/EmptyTensor.h>
+
+#include "TPUTorchUtils.h"
+
+#include "common/config.h"
+
+namespace at
+{
+inline at::Tensor & copysign_out_tpu(const at::Tensor & self, const at::Tensor & other, at::Tensor & out)
+{
+    CPU_IMPL_WARNING();
+    auto out_cpu = copysign( self.cpu(), other.cpu() );
+    out = out_cpu.to(out.device()).to(out.dtype());
+    return out;
+}
+
+inline at::Tensor & copysign_Scalar_out_tpu(const at::Tensor & self, const at::Scalar & other, at::Tensor & out)
+{
+    CPU_IMPL_WARNING();
+    auto out_cpu = copysign( self.cpu(), other );
+    out = out_cpu.to(out.device()).to(out.dtype());
+    return out;
+}
+
+inline at::Tensor copysign_Scalar_tpu(const at::Tensor & self, const at::Scalar & other)
+{
+    auto out = empty(self.sizes(), self.options());
+    return copysign_Scalar_out_tpu(self, other, out);
+}
+
+inline at::Tensor copysign_Tensor_tpu(const at::Tensor & self, const at::Tensor & other)
+{
+    auto out = empty(self.sizes(), self.options());
+    return copysign_out_tpu(self, other, out);
+}
+
+TORCH_LIBRARY_IMPL ( aten, TPU, m )
+{
+    m.impl ( "copysign.out",            copysign_out_tpu);
+    m.impl ( "copysign.Scalar_out",     copysign_Scalar_out_tpu);
+    m.impl ( "copysign.Scalar",         copysign_Scalar_tpu);
+    m.impl ( "copysign.Tensor",         copysign_Tensor_tpu);
+}
+} // namespace at
