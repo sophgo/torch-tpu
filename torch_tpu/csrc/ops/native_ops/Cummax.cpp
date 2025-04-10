9d206c8090058c1b4b184665da279d94cb14aad2
yu.hu <yu.hu@sophgo.com>
2025-04-10 09:59:28 +0800

    [register OP] cumsum,cumprod,cummax,cummin
    Change-Id: I85cb92c4bdcce9a1d62221a6391e0fb7d050bece

--- torch_tpu/csrc/ops/native_ops/Cummax.cpp
+++ torch_tpu/csrc/ops/native_ops/Cummax.cpp
@@ -6,9 +6,11 @@
 #include "TPUTorchUtils.h"
 #include "common/config.h"
 
-// called by https://pytorch.org/docs/2.1/generated/torch.cummax.html#torch.cummax
 namespace at{
-
+/**
+ * * * * * * * * * * * * * * * * * * * cumax * * * * * * * * * * * * * * * * * * 
+ */
+// called by https://pytorch.org/docs/2.1/generated/torch.cummax.html#torch.cummax
 std::tuple<Tensor &,Tensor &> cummax_out_tpu(const Tensor & self, int64_t dim, 
                                              Tensor & values, Tensor & indices) {
     CPU_IMPL_WARNING();
@@ -23,11 +25,71 @@ std::tuple<Tensor,Tensor> cummax_tpu(const Tensor & self, int64_t dim) {
     TensorOptions indices_opts = self.options().dtype(ScalarType::Int);
     auto values  = empty(self.sizes(), values_opts);
     auto indices = empty(self.sizes(), indices_opts); 
-    return _ops::cummax_out::call(self, dim, values, indices);
+    return cummax_out_tpu(self, dim, values, indices);
 }
 TORCH_LIBRARY_IMPL(aten, TPU, m)
 {
     m.impl("cummax.out", cummax_out_tpu);
     m.impl("cummax",     cummax_tpu);
+}
+
+/**
+ * * * * * * * * * * * * * * * * * * * cummin * * * * * * * * * * * * * * * * * * 
+ */
+// called by https://pytorch.org/docs/2.1/generated/torch.cummin.html#torch.cummin
+std::tuple<Tensor &,Tensor &> cummin_out_tpu(const Tensor & self, int64_t dim, 
+                                             Tensor & values, Tensor & indices) {
+    CPU_IMPL_WARNING();
+    auto outputs_cpu = torch::cummin(self.cpu(), dim);
+    values  = (std::get<0> (outputs_cpu)).to(values.device());
+    indices = (std::get<1> (outputs_cpu)).to(indices.device());
+    return {values, indices};
+}
+
+std::tuple<Tensor,Tensor> cummin_tpu(const Tensor & self, int64_t dim) {
+    TensorOptions values_opts  = self.options();
+    TensorOptions indices_opts = self.options().dtype(ScalarType::Int);
+    auto values  = empty(self.sizes(), values_opts);
+    auto indices = empty(self.sizes(), indices_opts); 
+    return cummin_out_tpu(self, dim, values, indices);
+}
+
+TORCH_LIBRARY_IMPL(aten, TPU, m)
+{
+    m.impl("cummin.out", cummin_out_tpu);
+    m.impl("cummin",     cummin_tpu);
+}
+
+/**
+ * * * * * * * * * * * * * * * * * * cumprod * * * * * * * * * * * * * * * * * * 
+ */
+// called by https://pytorch.org/docs/2.1/generated/torch.cumprod.html#torch.cumprod
+Tensor & cumprod_out_tpu(const Tensor & self, int64_t dim,
+                        c10::optional<ScalarType> dtype, Tensor & out) {
+    CPU_IMPL_WARNING();
+    auto out_cpu = torch::cumprod(self.cpu(), dim, dtype);
+    out = out_cpu.to(out.device());
+    return out;
+}
+TORCH_LIBRARY_IMPL(aten, TPU, m)
+{
+    m.impl("cumprod.out", cumprod_out_tpu);
+}
+/**
+ * * * * * * * * * * * * * * * * * * * cumsum * * * * * * * * * * * * * * * * * * 
+ */
+// called by https://pytorch.org/docs/2.1/generated/torch.cumsum.html#torch.cumsum
+Tensor & cumsum_out_tpu(const Tensor & self, int64_t dim, 
+                        c10::optional<ScalarType> dtype, Tensor & out) {
+    CPU_IMPL_WARNING();
+    auto out_cpu = torch::cumsum(self.cpu(), dim, dtype);
+    out = out_cpu.to(out.device());
+    return out;
+}
+// Tensor cumsum_tpu(const Tensor & self, int64_t dim, c10::optional<ScalarType> dtype=c10::nullopt) {}
+TORCH_LIBRARY_IMPL(aten, TPU, m)
+{
+    m.impl("cumsum.out", cumsum_out_tpu);
+    //m.impl("cumsum",     cumsum_tpu);
 }
 }; // namespace at
