6099cd2ae341a4e8d979664cd37cb791a5cefd31
yu.hu <yu.hu@sophgo.com>
2025-05-19 16:40:47 +0800

    Nonzero use cpu impl
    Change-Id: Id4a5b23e0c82ffde8e41ed6dcb26a2af2fd9f1ec

--- torch_tpu/csrc/ops/native_ops/Nonzero.cpp
+++ torch_tpu/csrc/ops/native_ops/Nonzero.cpp
@@ -10,11 +10,13 @@
 namespace at {
 
 Tensor nonzero_tpu(const Tensor &self) {
+#if 1
+  CPU_IMPL_WARNING();
+  auto out_cpu = nonzero(self.cpu());
+  Tensor out = out_cpu.to(self.device());
+#else
   CHECK_TENSOR_IN_DEVICE(self);
-  int size = 1;
-  for (int i=0; i<self.dim(); i++){
-    size *= self.size(i);
-  }
+  int size = self.numel();
   Tensor out_temp = empty({size, self.dim()}, self.options().dtype(kInt));
   Tensor num = empty({1}, self.options().dtype(kInt));
   TIMING_START;
@@ -28,12 +30,8 @@ Tensor nonzero_tpu(const Tensor &self) {
   TORCH_CHECK(status == TPUDNN_STATUS_SUCCESS);
     TIMING_END(tpu::NONZERO);
 
-  Tensor out = empty({num.item().toInt(), self.dim()}, self.options().dtype(kInt));
-  for (int i=0; i<num.item().toInt(); ++i){
-    for (int j=0; j<self.dim(); ++j){
-      out[i][j] = out_temp[i][j];
-    }
-  }
+  Tensor out = out_temp.index({Slice(0, num.item().toInt()), "..."});
+#endif
   // wait tpu support resize_
   // out.resize_((num.item().toInt(), self.dim()), c10::nullopt);
   SHOW_TENSOR_OP(self);
